0482a15b01bb99b0bc812a1b36d8f487e3684fe3
Abhiram <abhiram_cvns@hotmail.com>
2019-08-22 20:14:11 +0530

    Update practice.py
    wordcloud working

--- practice.py
+++ practice.py
@@ -10,12 +10,15 @@ import pandas as pd
 import json
 import matplotlib.pyplot as plt
 import seaborn as sns
+from os import path
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 business = pd.read_json("business.json", lines=True)
 checkin = pd.read_json("checkin.json", lines=True)
 tip = pd.read_json("tip.json", lines=True)
 user = pd.read_json("user.json", lines=True, chunksize=10000)
-reviews = pd.read_json("review.json", lines=True, chunksize=10000)
+review_reader = pd.read_json("review.json", lines=True, chunksize=10000)
 
 
 business.columns
@@ -26,14 +29,12 @@ Index(['address', 'attributes', 'business_id', 'categories', 'city', 'hours',
       dtype='object')
 '''
 
-review = pd.DataFrame(columns=['business_id', 'cool', 'date', 'funny', 'review_id', 'stars', 'text', 'useful', 'user_id'])
+
+review_df = pd.DataFrame(columns=['business_id', 'cool', 'date', 'funny', 'review_id', 'stars', 'text', 'useful', 'user_id'])
 
 
-
-
-for review in reviews:
-    review = review.append(review)
-
+for review in review_reader:
+    review_df = review_df.append(review)
 
 # for business id OPiPeoJiv92rENwbq76orA
 
@@ -46,10 +47,11 @@ review[['stars', 'text']][mask]
 users_less_than_3_rating = review.loc[review['stars'] <= 3]
 
 
+business['categories'] = business['categories'].str.split(pat = ', ?')
+
 categories = business['categories']
 
 
-business['categories'] = business['categories'].str.split(pat = ', ?')
 
 
 
@@ -68,107 +70,68 @@ restaurants = business.loc[business['categories'].apply(rest_cat)]
 
 
 
-
 restaurants_count = restaurants['categories'].value_counts().head(20)
 
 
 restaurants_count.plot(kind='bar')
 
 
+def pizzas(x):
+    return ('Pizza' in x) | ('Italian' in x)
 
+pizza_rest = business.loc[business['categories'].apply(pizzas)]
 
+pizza_rest.to_csv('business_list.csv', columns=['business_id', 'review_count', 'stars', 'state'], index=False)
 
+#tip:   ['business_id', 'compliment_count', 'date', 'text', 'user_id'], dtype='object')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-business.head()
-business['categories'] = business['categories'].str.split(pat = ', ?')
-
-category_counts = business['categories'].apply(pd.Series).stack().reset_index(drop=True).value_counts().head(20)
-
-
-def print_categories(data):
+'''
+#business: (['address', 'attributes', 'business_id', 'categories', 'city', 'hours',
+       'is_open', 'latitude', 'longitude', 'name', 'postal_code',
+       'review_count', 'stars', 'state'],
+      dtype='object')
     
-     categories_dict = {}
-     for i in range(0,20):    
-        val = data[i]
-        print(val)
-        if val in categories_dict:
-            categories_dict[val] = 1
-        else:
-            categories_dict[val] = 1
-            
-     return categories_dict
-    
- 
-        
-business = business.loc[~pd.isna(business['categories'])]
+review: 
 
+['business_id', 'cool', 'date', 'funny', 'review_id', 'stars', 'text',
+       'useful', 'user_id'],
+      dtype='object'    
+'''
 
-business = business.drop
+businesses = pd.read_csv('business_list.csv')
+business_ids = businesses['business_id'].values
 
-def process(data):
-    mask = []
-    for i in range(len(data)):
-        if 'Restaurants' in data[i]:
-            mask.append(True)
-        else:
-            mask.append(False)
-    return mask
+#reviews_updated = [review.loc[review['business_id'].apply(lambda x: x in business_ids)] for review in review_reader]
 
-business_updated = process(business['categories'])
-business['categories'][business_updated]
+def pizzas_review(x):
+    return x in business_ids
+
+pizza_reviews = review_df.loc[review_df['business_id'].apply(pizzas_review)]
 
 
 
-def lambdafunc(x):
-    return "Restaurants" in x
 
-z = business.loc[business['categories'].apply(lambdafunc),:]
+good_reviews = pizza_reviews.loc[pizza_reviews['stars'] == 5]
+bad_reviews = pizza_reviews.loc[pizza_reviews['stars'] <= 4]
 
-business = business.loc[~pd.isna(business['categories'])]
-italian_pizza = business.loc[business['categories'].apply(lambda x: ('Pizza' in x) | ('Italian' in x))]
-italian_pizza.to_csv('business_data.csv', columns=['business_id', 'review_count', 'stars', 'state'], index=False)
+a_user = good_reviews.iloc[1]['user_id']
 
+pizza_reviews.loc[pizza_reviews['user_id'] == 'tU-4DhNg8JjCHwzzNe3ojQ']
 
 
-def find(data):
-    categories_dict = {}
-    
-    for i in range(500):
-        print(data[i])
-        for j in range(len(data[i])):    
-            if data[i][j] in categories_dict:
-                categories_dict[data[i][j]] = 1
-            else:
-                categories_dict[data[i][j]] =+ 1
-            
-    return categories_dict
-            
-   
-new = find(business['categories'])        
-    
-    
 
-def print_categories(data):
-    
-    print(len(data))
- 
-        
-new = business['categories'].apply(print_categories)
+
+
+
+
+
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, r'C:\Users\abhiram_ch_v_n_s\Desktop\yelp_casestudy\yelp_dataset\wordcloud')
+
+
+
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 
 
@@ -180,6 +143,16 @@ new = business['categories'].apply(print_categories)
 
 
 
-  
 
-business['categories'].head()
+
+
+
+
+
+
+
+
+
+
+
+
